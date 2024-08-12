276aaa37559fdae1a251e53ece2d9dd6a2c86229
Dean Minnie <dminnie33@gmail.com>
2024-08-12 09:56:05 +0200

    Date and time only displays if not in tmux

--- nvim/.config/nvim/lua/chadrc.lua
+++ nvim/.config/nvim/lua/chadrc.lua
@@ -6,24 +6,40 @@ M.ui = {
   theme = "catppuccin",
   theme_toggle = { "catppuccin", "catppuccin" },
 
-  statusline = {
-    order = {"mode", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cwd", "cursor", "time", "date"},
-    
-    modules = {
-      time = function()
-        return "%#St_time_sep#%#St_time_icon#󰥔 %#St_time_text#" .. os.date " %H:%M "
-      end,
-      date = function()
-        return "%#St_date_sep#%#St_date_icon#𝍄 %#St_date_text#" .. os.date " %d/%m/%Y "
-      end
-    
-    }
-  },
+  -- statusline = {
+  --   order = {"mode", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cwd", "cursor", "time", "date"},
+  --
+  --   modules = {
+  --     time = function()
+  --       return "%#St_time_sep#%#St_time_icon#󰥔 %#St_time_text#" .. os.date " %H:%M "
+  --     end,
+  --     date = function()
+  --       return "%#St_date_sep#%#St_date_icon#𝍄 %#St_date_text#" .. os.date " %d/%m/%Y "
+  --     end
+  --
+  --   }
+  -- },
   tabufline = {
-      lazyload = false,
+    lazyload = false,
   },
   hl_override = highlights.override,
   hl_add = highlights.add,
 }
+
+if not os.getenv "TMUX" then
+  M.ui.statusline = {
+      order = {"mode", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cwd", "cursor", "time", "date"},
+
+      modules = {
+        time = function()
+          return "%#St_time_sep#%#St_time_icon#󰥔 %#St_time_text#" .. os.date " %H:%M "
+        end,
+        date = function()
+          return "%#St_date_sep#%#St_date_icon#𝍄 %#St_date_text#" .. os.date " %d/%m/%Y "
+        end
+
+      }
+  }
+end
 
 return M
