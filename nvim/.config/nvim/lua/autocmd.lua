e3412794f8db81452d786883563be8287e6cd2a7
Dean Minnie <dminnie33@gmail.com>
2025-04-30 09:58:04 +0200

    Remove trailing white spaces

--- nvim/.config/nvim/lua/autocmd.lua
+++ nvim/.config/nvim/lua/autocmd.lua
@@ -19,3 +19,12 @@ vim.api.nvim_create_autocmd({ "VimEnter" }, {
     require("nvim-tree.api").tree.toggle { focus = true, find_file = true }
   end,
 })
+
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+    pattern = {"*"},
+    callback = function()
+      local save_cursor = vim.fn.getpos(".")
+      pcall(function() vim.cmd [[%s/\s\+$//e]] end)
+      vim.fn.setpos(".", save_cursor)
+    end,
+})
