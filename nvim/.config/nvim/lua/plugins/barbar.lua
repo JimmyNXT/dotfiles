1226762dac42953ee64240b649de553932ae4b05
Dean Minnie <dminnie33@gmail.com>
2025-09-18 11:26:10 +0200

    Top bar key maps

--- nvim/.config/nvim/lua/plugins/barbar.lua
+++ nvim/.config/nvim/lua/plugins/barbar.lua
@@ -4,11 +4,18 @@ return {
     'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
     'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
   },
-  init = function() vim.g.barbar_auto_setup = false end,
+  init = function()
+    vim.g.barbar_auto_setup = false
+  end,
   opts = {
     sidebar_filetypes = {
       ['neo-tree'] = true, -- { event = 'BufWipeout' },
     },
+    maximum_length = 30,
   },
-  version = '^1.0.0', -- optional: only update when a new 1.x version is released
+  config = function(_, opts)
+    require('barbar').setup(opts)
+    vim.keymap.set('n', '<Tab>', '<cmd>BufferNext<CR>', { desc = 'Move to next buffer' })
+    vim.keymap.set('n', '<leader>x', '<cmd>BufferClose<CR>', { desc = 'Close current buffer' })
+  end,
 }
