9c1180ebe9529cc52b679879dc8bb0aec3735933
Dean Minnie <dminnie33@gmail.com>
2025-09-22 11:15:39 +0200

    Home and end keys

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -37,3 +37,6 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
+
+vim.keymap.set({'n', 'v', 'i', 'c'}, '', '<Home>')
+vim.keymap.set({'n', 'v', 'i', 'c'}, '', '<End>')
