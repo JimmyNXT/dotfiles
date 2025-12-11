ac9be07a647f6607a5bb3ef98b7ce791d47f5f73
dem <dem@nanoteq.net>
2025-12-11 12:20:38 +0200

    Feat: Added LLDB config

--- nvim/.config/nvim/lua/plugins/debug.lua
+++ nvim/.config/nvim/lua/plugins/debug.lua
@@ -144,21 +144,49 @@ return {
                 detached = vim.fn.has 'win32' == 0,
             },
         }
-        dap.adapters.gdb = {
-            type = 'executable',
-            command = 'gdb',
-            args = { '--interpreter=dap', '--eval-command', 'set print pretty on' },
-        }
+        function split_respecting_quotes(str)
+            local result = {}
+            local in_quotes = false
+            local current = ""
+            
+            for i = 1, #str do
+                local char = str:sub(i, i)
+                
+                if char == '"' then
+                    in_quotes = not in_quotes
+                elseif char == ' ' and not in_quotes then
+                    if current ~= "" then
+                        table.insert(result, current)
+                        current = ""
+                    end
+                else
+                    current = current .. char
+                end
+            end
+            
+            if current ~= "" then
+                table.insert(result, current)
+            end
+            
+            return result
+        end
+
+        -- dap.adapters.gdb = {
+        --     type = 'executable',
+        --     command = 'gdb',
+        --     args = { '--interpreter=dap', '--eval-command', 'set print pretty on' },
+        --     name = 'gdb'
+        -- }
 
         dap.adapters.lldb = {
             type = 'executable',
-            command = 'lldb',
-            args = { '--interpreter=dap', '--eval-command', 'set print pretty on' },
+            command = 'lldb-dap', -- remember to sim link lldb-dap-[someting] or lldb-vscode[something] to lldb-dap
+            name= 'lldb'
         }
 
         dap.configurations.cpp = {
             {
-                name = 'Launch (LLDB)',
+                name = '(LLDB) Launch',
                 type = 'lldb',
                 request = 'launch',
                 program = function()
@@ -169,39 +197,52 @@ return {
                 args = {},
             },
             {
-                name = 'Launch (GDB)',
-                type = 'gdb',
+                name = '(LLDB) Launch with args',
+                type = 'lldb',
                 request = 'launch',
                 program = function()
                     return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
                 end,
-                args = {}, -- provide arguments if needed
                 cwd = '${workspaceFolder}',
-                stopAtBeginningOfMainSubprogram = false,
-            },
-            {
-                name = 'Select and attach to process (GDB)',
-                type = 'gdb',
-                request = 'attach',
-                program = function()
-                    return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+                stopOnEntry = false,
+                args = function()
+                    return split_respecting_quotes(vim.fn.input('Args: ', ""))
                 end,
-                pid = function()
-                    local name = vim.fn.input 'Executable name (filter): '
-                    return require('dap.utils').pick_process { filter = name }
-                end,
-                cwd = '${workspaceFolder}',
             },
-            {
-                name = 'Attach to gdbserver :1234',
-                type = 'gdb',
-                request = 'attach',
-                target = 'localhost:1234',
-                program = function()
-                    return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-                end,
-                cwd = '${workspaceFolder}',
-            },
+            -- {
+            --     name = 'Launch (GDB)',
+            --     type = 'gdb',
+            --     request = 'launch',
+            --     program = function()
+            --         return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+            --     end,
+            --     args = {}, -- provide arguments if needed
+            --     cwd = '${workspaceFolder}',
+            --     stopAtBeginningOfMainSubprogram = false,
+            -- },
+            -- {
+            --     name = 'Select and attach to process (GDB)',
+            --     type = 'gdb',
+            --     request = 'attach',
+            --     program = function()
+            --         return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+            --     end,
+            --     pid = function()
+            --         local name = vim.fn.input 'Executable name (filter): '
+            --         return require('dap.utils').pick_process { filter = name }
+            --     end,
+            --     cwd = '${workspaceFolder}',
+            -- },
+            -- {
+            --     name = 'Attach to gdbserver :1234',
+            --     type = 'gdb',
+            --     request = 'attach',
+            --     target = 'localhost:1234',
+            --     program = function()
+            --         return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+            --     end,
+            --     cwd = '${workspaceFolder}',
+            -- },
         }
         dap.configurations.c = dap.configurations.cpp
     end,
