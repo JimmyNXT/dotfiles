5ed3d01aa9bd0222889bd795f883e70914ec3602
Dean Minnie <dminnie33@gmail.com>
2025-12-15 18:24:41 +0200

    Obsidian api

--- nvim/.config/nvim/lua/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -1,18 +1,36 @@
 return {
-  "epwalsh/obsidian.nvim",
+  -- 'epwalsh/obsidian.nvim',
+  'obsidian-nvim/obsidian.nvim',
+  version = '*',
+  ft = 'markdown',
   lazy = true,
-  ft = "markdown",
   dependencies = {
-    "nvim-lua/plenary.nvim",
-    "hrsh7th/nvim-cmp",
-    "nvim-telescope/telescope.nvim",
-    "nvim-treesitter/nvim-treesitter",
+    'nvim-lua/plenary.nvim',
+    'saghen/blink.cmp',
+    'nvim-telescope/telescope.nvim',
+    'nvim-treesitter/nvim-treesitter',
   },
-  opts = function()
-    return require "configs.obsidian"
-  end,
+  event = {
+    'BufReadPre ' .. vim.fn.expand '~' .. '/vaults/**.md',
+    'BufNewFile ' .. vim.fn.expand '~' .. '/vaults/**.md',
+  },
+  -- @module 'obsidian'
+  -- @type obsidian.config
+  opts = {
+    legacy_commands = false,
+    workspaces = {
+      {
+        name = 'personal',
+        path = '~/vaults/personal',
+      },
+      {
+        name = 'work',
+        path = '~/vaults/professional',
+      },
+    },
+  },
   config = function(_, opts)
     vim.o.conceallevel = 1
-    require("obsidian").setup(opts)
+    require('obsidian').setup(opts)
   end,
 }
